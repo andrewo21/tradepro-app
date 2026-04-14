fbe81457a0c36834314c9b3cc4028ae4d288dfe4
Andrew ONeill <andrew.oneill79@gmail.com>
2026-04-14 13:18:33 -0300

    add fix to server.js

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
