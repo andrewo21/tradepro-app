f81173fe72643c2d848ddd0e5c60a64da763c8d8
Andrew ONeill <andrew.oneill79@gmail.com>
2026-04-19 13:39:14 -0300

    fiz to server

--- pdf-service/server.js
+++ pdf-service/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { chromium } from "@playwright/test";
+import { chromium } from "playwright";
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
