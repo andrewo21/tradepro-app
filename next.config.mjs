4b40bde770d2669e98cf1c455371e047218371df
Andrew ONeill <andrew.oneill79@gmail.com>
2026-04-21 22:45:30 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["@resvg/resvg-wasm"],
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "webassembly/async",
-    });
-    return config;
   },
 };
 
